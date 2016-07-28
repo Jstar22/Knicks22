aa5b00afe7f06edf9d510d4002036c90461d070f
hecto <hecto@10.0.36.35>
2016-07-28 14:16:46 -0400

    Login Page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <h1> Bookzan </h1>
+            User Name:<br>
+            <input> 
+            <br>
+            
+            Password:<br>
+            <input> 
+            <br><br>
+              <input type="submit" value="Submit">
+</form>
+        </form>
+    </body>
+</html>
